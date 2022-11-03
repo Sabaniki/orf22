ccd2a991e077dc1612f30ea77c7a2cc3a5d8f29b
Sabaniki <sabaniki@sfc.wide.ad.jp>
2022-11-03 15:28:22 +0000

    refactor

--- src/handler/vlan.rs
+++ src/handler/vlan.rs
@@ -1,18 +1,14 @@
 use pnet::packet::ethernet::{EtherTypes,EthernetPacket, EtherType};
 use pnet::packet::vlan::VlanPacket;
-use pnet::packet::ip::{IpNextHeaderProtocols, IpNextHeaderProtocol};
-use pnet::packet::ipv4::Ipv4Packet;
-use pnet::packet::ipv6::Ipv6Packet;
 use pnet::packet::Packet;
-use crate::handler::transport::tcp_handler;
 use crate::handler::ip::v4_handler;
 use crate::handler::ip::v6_handler;
-use crate::packet::ip::L3Packet;
 use crate::packet::tuples::FiveTupleWithFlagsAndTime;
 
 // Ether のペイロードから IPv4 パケットを抽出．次のレイヤのハンドラを呼び出す
 pub fn vlan_handler(ethernet: &EthernetPacket) -> Option<FiveTupleWithFlagsAndTime> {
     if let Some(vlan) = VlanPacket::new(ethernet.payload()) {
+        debug!("vlan id: {}", vlan.get_vlan_identifier());
         return call_ip_handler(&vlan, vlan.get_ethertype());
     }
     None
