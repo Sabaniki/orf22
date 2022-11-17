39c9f6c6210d871933e5c4b61de90946f9f86fbd
Sabaniki <sabaniki@sfc.wide.ad.jp>
2022-11-17 02:24:55 +0000

    file write

--- src/util/app.rs
+++ src/util/app.rs
@@ -7,33 +7,41 @@ fn create_app() -> ArgMatches {
         .author("sabaniki")
         .about("this is passive rtt mesuarement application written in Rust.")
         .arg(
+            Arg::new("net_name")
+                .value_name("net_name")
+                .index(1)
+                .required(true),
+        )
+        .arg(
             Arg::new("interface_name")
                 .value_name("interface_name")
-                .index(1)
+                .index(2)
                 .required(true),
         )
         .arg(
             Arg::new("ipv4_range")
                 .value_name("ipv4_range")
-                .index(2)
+                .index(3)
                 .required(true),
         )
         .arg(
             Arg::new("ipv6_range")
                 .value_name("ipv6_range")
-                .index(3)
+                .index(4)
                 .required(true),
         )
         .get_matches()
 }
 
-pub fn get_arg() -> Result<(String, String, String), anyhow::Error> {
+pub fn get_arg() -> Result<(String, String, String, String), anyhow::Error> {
     let app = create_app();
+    let net_name = app.value_of("net_name")
+        .with_context(||"could not get the arg [net_name]")?;
     let interface_name = app.value_of("interface_name")
         .with_context(||"could not get the arg [interface_name]")?;
     let ipv4_range = app.value_of("ipv4_range")
         .with_context(||"could not get the arg [ipv4_range]")?;
     let ipv6_range = app.value_of("ipv6_range")
         .with_context(||"could not get the arg [ipv6_range]")?;
-    Ok((interface_name.to_string(), ipv4_range.to_string(), ipv6_range.to_string()))
+    Ok((net_name.to_string(), interface_name.to_string(), ipv4_range.to_string(), ipv6_range.to_string()))
 }
